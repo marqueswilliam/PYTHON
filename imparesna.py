a81927d4104f2e8c9c95e7c0241005266d93bbef
William Marques <william.marquesousa@gmail.com>
2017-12-21 10:24:54 -0200

    PY 03

---
+++ imparesna.py
@@ -0,0 +1,12 @@
+num = int(input("Numero: "))
+parada = 0
+impares = 0
+
+while parada < num:
+    impares += 1
+    if impares % 2 == 0:
+        pass
+
+    else:
+        print impares
+        parada += 1
